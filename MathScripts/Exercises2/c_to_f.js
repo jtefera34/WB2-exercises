5e444e15a8a1f7134f5bb173f7d836e4e84b3ad4
Yoseph Tefera <jtefera34@gmail.com>
2024-04-23 12:08:17 -0400

    temprature formula

---
+++ MathScripts/Exercises2/c_to_f.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// sample inputs
+var fahr;
+var cels = 30;
+
+// calculations
+var fahr = (cels * 1.8)+32
+// output
+console.log(fahr);
---
+++ MathScripts/Exercises2/f_to_c.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// sample inputs
+var fahr = 30;
+var cels;
+
+// calculations
+var cels = (fahr - 32)/1.8
+// output
+console.log(cels);
