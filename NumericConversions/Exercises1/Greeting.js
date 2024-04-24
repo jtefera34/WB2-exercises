96550e8a8237c2b7998aabcf0130ec3a49d5369c
Yoseph Tefera <jtefera34@gmail.com>
2024-04-24 15:22:59 -0400

    till Complex taxes

---
+++ NumericConversions/Exercises1/Greeting.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var hour = 9;
+var greeting;
+
+
+if (hour <= 10){
+  greeting = "Good Morning";
+}
+else if (hour >= 17){
+  greeting = "Good Evening";
+}
+
+else if (hour >= 10 && 10 <= 16.59){
+  greeting = "Good day";
+}
+
+
+ console.log(greeting)
---
+++ NumericConversions/pay_rules.js
@@ -0,0 +1,26 @@
+"use strict"
+
+var payrate;
+var hoursworked = 20 ;
+var Grosspay;
+
+Grosspay = payrate * hoursworked
+
+if (hoursworked > 40) {
+  payrate = 17.3 ;
+}
+
+else if
+  (hoursworked < 40){
+  payrate = 12.50;
+}
+
+else{
+  (hoursworked = 40)
+  payrate = 25;
+}
+
+
+
+Grosspay = payrate * hoursworked
+console.log(Grosspay)
